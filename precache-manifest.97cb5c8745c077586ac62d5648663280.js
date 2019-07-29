87c43afd69f3b3f15bd81fe481448f3f3b8f5f6c
rkfr <e.xbyvnqn@gmail.com>
2019-07-29 17:00:04 +0200

    Updates

---
+++ precache-manifest.97cb5c8745c077586ac62d5648663280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b73e90f0deabc1f62e5f7201659e719",
+    "url": "/dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "1c4a6bda8d5d21c06dcd",
+    "url": "/dynamic-list-of-posts/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dynamic-list-of-posts/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1c4a6bda8d5d21c06dcd",
+    "url": "/dynamic-list-of-posts/static/js/main.8cce30af.chunk.js"
+  },
+  {
+    "revision": "673aa693b1fbb0657bfb",
+    "url": "/dynamic-list-of-posts/static/js/runtime~main.da5f0574.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dynamic-list-of-posts/static/media/logo.5d5d9eef.svg"
+  }
+]);
